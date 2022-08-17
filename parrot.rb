9e25d204c2148a5969a26324307b39a9e64bf9c5
Jobsidney <jobsidney67@gmail.com>
2022-08-17 15:58:41 +0300

    DOne with assignment

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,20 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(str="Squawk!")
+    unless str.empty?
+        puts str
+        str
+    else
+        puts "Squawk!"
+    end
+end
+parrot()
+
+
+
+# if str=="parrot"
+#     puts "Squawk!"
+# else
+#     puts str
+#     str
+# end
